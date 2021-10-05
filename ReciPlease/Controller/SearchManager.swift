1b05ecec071477b4f1b8d10c237155d7d1bbdf27
Adrien <adrien@perea.be>
2021-10-05 16:16:59 +0200

    mise en place alamofire, manager, delegate

---
+++ ReciPlease/Controller/SearchManager.swift
@@ -0,0 +1,8 @@
+//
+//  SearchManager.swift
+//  ReciPlease
+//
+//  Created by Adrien PEREA on 05/10/2021.
+//
+
+import Foundation
---
+++ ReciPlease/Model/APIResponse.swift
@@ -0,0 +1,8 @@
+//
+//  APIResponse.swift
+//  ReciPlease
+//
+//  Created by Adrien PEREA on 05/10/2021.
+//
+
+import Foundation
